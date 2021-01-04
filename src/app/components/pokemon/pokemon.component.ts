8062a36b95ad8c3ed1f142fe2e00af759f280643
listgaus <listgaus@gmail.com>
2021-01-04 20:33:08 +0200

    Update pokemon.component.ts

--- src/app/components/pokemon/pokemon.component.ts
+++ src/app/components/pokemon/pokemon.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { Pokemon } from '../../models/pokemon';
 import { PokeshopStore } from '../../services/app.store';
 import { DialogService } from '../../services/dialog.service';
