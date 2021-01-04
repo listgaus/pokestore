ded35b3055d613849942d31a1588de80a1b5cf9d
listgaus <listgaus@gmail.com>
2021-01-04 15:58:02 +0200

    Update pokemon.component.ts

--- src/app/components/pokemon/pokemon.component.ts
+++ src/app/components/pokemon/pokemon.component.ts
@@ -1,44 +1,44 @@
-import {Component, ElementRef, Input, ViewChild} from '@angular/core';
-import {Pokemon} from '../../models/pokemon';
-import {LoggerService} from '../../services/logger.service';
-import {PokeshopStore} from '../../services/app.store';
-import {DialogService} from '../../services/dialog.service';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Pokemon } from '../../models/pokemon';
+import { LoggerService } from '../../services/logger.service';
+import { PokeshopStore } from '../../services/app.store';
+import { DialogService } from '../../services/dialog.service';
 
 @Component({
   selector: 'pokemon-card',
   templateUrl: './pokemon.component.html',
   styleUrls: ['./pokemon.component.scss'],
 })
-export class PokemonComponent{
+export class PokemonComponent {
   @Input() pokemon: Pokemon;
   @Input() inCart = false;
-  @ViewChild('pokeCard') pokeCard;
   @ViewChild('pokeImg') pokeImg;
-
-  constructor(
-    private logger: LoggerService,
-    private store: PokeshopStore,
-    private dialogRef: DialogService,
-  ) {
-  }
+  animationDelay = 1500;
+  
+  constructor(private store: PokeshopStore, private dialogRef: DialogService) {}
 
   addToCart() {
     this.pokeImg.nativeElement.classList.add('sendtocart');
     setTimeout(() => {
       this.pokeImg.nativeElement.classList.remove('sendtocart');
-    }, 1500);
+    }, this.animationDelay);
     this.store.addToCart(this.pokemon);
   }
 
   removeFromCart() {
-    this.dialogRef.openConfirmDialog(`are you sure you wanna remove ${this.pokemon.name} from the cart?`).afterClosed().subscribe(res => {
-      if (res) {
-        this.pokeImg.nativeElement.classList.add('removefromcart');
-        setTimeout(() => {
-          this.pokeImg.nativeElement.classList.remove('removefromcart');
-          this.store.removeFromCart(this.pokemon);
-        }, 1500);
-      }
-    });
+    this.dialogRef
+      .openConfirmDialog(
+        `are you sure you wanna remove ${this.pokemon.name} from the cart?`
+      )
+      .afterClosed()
+      .subscribe((res) => {
+        if (res) {
+          this.pokeImg.nativeElement.classList.add('removefromcart');
+          setTimeout(() => {
+            this.pokeImg.nativeElement.classList.remove('removefromcart');
+            this.store.removeFromCart(this.pokemon);
+          }, this.animationDelay);
+        }
+      });
   }
 }
